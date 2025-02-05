56fb50f40dd844d8f2dbb88e1be5f8aedfc19465
Jonathan Duggan <jonathanduggan2002@gmail.com>
2025-02-04 22:15:22 -0500

    smoke test for Python Flask and HTML page, Run on localhost:5000

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,1 +1,15 @@
-print("test")
+from flask import Flask, render_template
+import logging
+
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+    
+def main():
+    logging.info("Starting Python FLask")
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
