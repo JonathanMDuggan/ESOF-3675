56c43e0f7e2b235c48005c95e16b765b2c1a2b25
Jonathan Duggan <jonathanduggan2002@gmail.com>
2025-02-08 03:19:50 -0500

    gonna work on this in the morning

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
-import logging
+import logging , base64
+import json
 from spotipy.oauth2 import SpotifyOAuth
+from requests import post
 
 app = Flask(__name__)
 
@@ -14,25 +16,35 @@ def index():
 def name():
     return 'You typed home in the funny thing <home>'
 
-#def print_saved_tracks():
-#    scope = "user-library-read"     
-#    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))   
-#    results = sp.current_user_saved_tracks()
-#    for i, item in enumerate(result['items']):
-#        track = item['track']
-#        print(i, track['artists'][0]['name'], " - " track['name'])
-#
+def spotify_load_api():
+    return load_api("SPOTIFY_CLIENT_ID", "SPOTIFY_CLIENT_SECERT", "https://accounts.spotify.com/api/token")
 
 # Load the api from the .env file
-def load_api(API_CLIENT_ID_NAME: str, API_CLIENT_SECERT_NAME: str):
+def load_api(API_CLIENT_ID_NAME: str, API_CLIENT_SECERT_NAME: str, URL: str):
     CLIENT_ID     = os.getenv(API_CLIENT_ID_NAME)
     CLIENT_SECERT = os.getenv(API_CLIENT_SECERT_NAME)
+
     if CLIENT_ID == None or CLIENT_SECERT == None:
         logging.error("%s is: %s", API_CLIENT_ID_NAME, CLIENT_ID)
         logging.error("%s is: %s", API_CLIENT_SECERT_NAME, CLIENT_SECERT)
+        return None
     else:
         logging.info("Successfully loaded %s and %s", API_CLIENT_ID_NAME, API_CLIENT_SECERT_NAME)
-    return CLIENT_ID, CLIENT_SECERT
+
+    AUTH_STRING = CLIENT_ID + ":" + CLIENT_SECERT
+    AUTH_BYTES = AUTH_STRING.encode("utf-8")
+    AUTH_BYTES64 = str(base64.b64encode(AUTH_BYTES), "utf-8")
+
+    headers = {
+        "Authorization": "Basic " + AUTH_BYTES64,
+        "Content-Type:": "application/x-www-form-urlencoded"
+    }
+
+    data = {"grant_type": "client_credentials"}
+    result = post(URL, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
     
 
 def main():
@@ -41,7 +53,7 @@ def main():
     logging.info("Starting Python FLask")
     # load the spotify api
     load_dotenv()
-    SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECERT = load_api("SPOTIFY_CLIENT_ID", "SPOTIFY_CLIENT_SECERT")
+    spotify_load_api()
     app.run(debug=True)
 
 if __name__ == '__main__':
---
+++ backend/src/spotify_api.py
@@ -0,0 +1,17 @@
+from src.main import load_api
+import logging
+from dotenv import load_dotenv
+
+class SpotifyAPI:
+    def __init__(self, API_CLIENT_ID_NAME, API_CLIENT_SECERT_NAME):
+        load_dotenv()
+        self.token = load_api(API_CLIENT_ID_NAME, API_CLIENT_SECERT_NAME,
+                 "https://accounts.spotify.com/api/token" )
+        if self.token == None:
+            logging.error("Failed to fetch token from spotify servers")
+        else:
+            self.auth_header = {"Authorization": "Bearer " + self.token}
+        return
+    
+    def get_artist(artist_name : str):
+        
