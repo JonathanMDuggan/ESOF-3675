8ff1448d570ed37815bf6a617a9053f9a175b1f9
Jonathan Duggan <jonathanduggan2002@gmail.com>
2025-02-09 04:20:36 -0500

    going to add youtube api to the system too

--- backend/src/youtube_func.py
+++ backend/src/youtube_func.py
@@ -3,8 +3,12 @@ import logging
 import json
 from dotenv import load_dotenv
 from requests import get, post, exceptions
+from googleapiclient.discovery import build
+import os
 
 class YoutubeAPIFacade:
-    def __init__(self, API_CLIENT_ID_NAME, API_CLIENT_SECRET_NAME):
-
+    def __init__(self, API_CLIENT_ID_NAME):
+        load_dotenv()
+        API_KEY = os.getenv(API_CLIENT_ID_NAME)
+        self.service = build('youtube', 'v3', developerKey=API_KEY)
         pass
